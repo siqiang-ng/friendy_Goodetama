6fd53a2bbd5ce58b3b66ea900fb9d49b43d1cfb0
YJ048 <yangjing.nus@gmail.com>
2021-01-08 19:37:31 +0800

    created container for homepage

--- lib/homePage/homeBody.dart
+++ lib/homePage/homeBody.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:friendy/components/background.dart';
+import 'package:friendy/style/palette.dart';
 
-class HomeBody extends StatelessWidget {
+class HomeBody extends StatefulWidget {
+  @override
+  _HomeBodyState createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Background(
-      body: Container(
-        child: Center(
-          child: Text("Home Page"),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.fromLTRB(10.0, 1.0, 5.0, 1.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Palette.kDarkBlue,
+              ),
+              child: Text("Container 1",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.fromLTRB(10.0, 1.0, 5.0, 1.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Palette.kLightBlue,
+              ),
+              child: Text("Container 2",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
