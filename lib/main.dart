e62ffa2eb7faae919a4041ba4c560964995f5f10
siqiang-ng <nsq.siqiang@gmail.com>
2021-01-08 14:26:47 +0800

    Rename imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/siqiang/OneDrive/Desktop/friendy_Goodetama/lib/loginPage/loginPage.dart';
+import 'package:friendy/loginPage/loginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
